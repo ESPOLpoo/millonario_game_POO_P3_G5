0d70d2671265b1c35a806dc6b86ab5efcbcec64a
anthleonESPOL <anthleon@espol.edu.ec>
2023-07-13 17:02:44 -0500

    Clases Proyecto POO

---
+++ TerminoAcademico.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class TerminoAcademico {
+
+    //ATRIBUTOS
+    private int año;
+    private int numTermino;
+    private ArrayList<Materia> materias;
+    private ArrayList<Participante> estudiantes;
+
+    //CONSTRUCTOR
+    public TerminoAcademico(int año, int numTermino){
+        this.año = año;
+        this.numTermino = numTermino;
+    }
+
+    //GETTERS
+    public int getAño() {
+        return año;
+    }
+    public int getNumTermino() {
+        return numTermino;
+    }
+    public ArrayList<Materia> getMaterias() { return materias; }
+    public ArrayList<Participante> getEstudiantes() { return estudiantes; }
+
+    //SETTERS
+    public void setAño(int año) { this.año = año; }
+    public void setNumTermino(int numTermino) { this.numTermino = numTermino; }
+
+}

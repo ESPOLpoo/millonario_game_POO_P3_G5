b4208c4b323fd319c698ab2c3a4d54501ac4cf88
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-07-16 21:50:29 -0500

    [ADD] data as specified on doc

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -93,7 +93,7 @@ public class Main {
         Materia materia2 = new Materia("MATG1046", "CÁLCULO VECTORIAL", 2);
         Materia materia3 = new Materia("MATG1050", "ECUACIONES DIFERENCIALES", 1);
         Materia materia4 = new Materia("MATG1049", "ÁLGEBRA LINEAL", 1);
-        Materia materia5 = new Materia("CCPG1052", "PROGRAMACIÓN ORIENTADA A OBJETOS", 1);
+        Materia materia5 = new Materia("CCPG1052", "PROGRAMACIÓN ORIENTADA A OBJETOS", 2);
 
         Paralelo paraleloCUVP1 = new Paralelo(TerminoAcademico.getTermino("2021-1"), materia1, 1);
         ArrayList<Estudiante> estudiantesCUVP1 = new ArrayList<>();
@@ -193,7 +193,46 @@ public class Main {
         estudiantesPOOP14.add(new Estudiante("Andrea Morales", "202512347"));
         paraleloPOOP14.setEstudiantes(estudiantesPOOP14);
 
-        Collections.addAll(Paralelo.paralelos, paraleloCUVP1, paraleloCUVP2, paraleloCUVP3, paraleloCUVP4, paraleloCVP5, paraleloCVP6, paraleloCVP7, paraleloEDP8, paraleloEDP9, paraleloALP10, paraleloALP11, paraleloPOOP12, paraleloPOOP13, paraleloPOOP14);
+        Paralelo paraleloPOO3 = new Paralelo(TerminoAcademico.getTermino("2023-1"), materia5, 3);
+        ArrayList<Estudiante> estudiantesPOO3 = new ArrayList<>();
+        estudiantesPOO3.add(new Estudiante("ACELDO TORRES MARIA GRAZIA", "202500001"));
+        estudiantesPOO3.add(new Estudiante("AGUILAR TINOCO JEAN CARLOS", "202500002"));
+        estudiantesPOO3.add(new Estudiante("AMORETTI SANCHEZ JUAN CARLOS", "202500003"));
+        estudiantesPOO3.add(new Estudiante("ANDRADE VELASCO ANGELLO BERNIE", "202500004"));
+        estudiantesPOO3.add(new Estudiante("ARAUJO ORTEGA DIEGO ENZO JAVIER", "202500005"));
+        estudiantesPOO3.add(new Estudiante("AZU PERLAZA NICOLE FERNANDA", "202500006"));
+        estudiantesPOO3.add(new Estudiante("BALDEON BAQUE IVAN GONZALO", "202500007"));
+        estudiantesPOO3.add(new Estudiante("BARBERAN GALLARDO MELISSA ESTEFANIA", "202500008"));
+        estudiantesPOO3.add(new Estudiante("BASILIO ACEBO DANIELA MILENA", "202500009"));
+        estudiantesPOO3.add(new Estudiante("BORBOR GUTIERREZ VICTOR DANIEL", "202500010"));
+        estudiantesPOO3.add(new Estudiante("CABRERA VIVANCO ALVARO DAVID", "202500011"));
+        estudiantesPOO3.add(new Estudiante("CARRILLO BASTIDAS GLADYS ELIANA", "202500012"));
+        estudiantesPOO3.add(new Estudiante("CORDERO CALLES RONALD ELIAS", "202500013"));
+        estudiantesPOO3.add(new Estudiante("ESPINOZA PINELA ANGELO ALEXANDER", "202500014"));
+        estudiantesPOO3.add(new Estudiante("GONZABAY ESPIN DOUGLAS VICENTE", "202500015"));
+        estudiantesPOO3.add(new Estudiante("GUAMAN QUIJIJE RONALD STEVEN", "202500016"));
+        estudiantesPOO3.add(new Estudiante("HERRERA LEON ANTHONY ARTURO", "202500017"));
+        estudiantesPOO3.add(new Estudiante("LINO INDACOCHEA STEVEN MOISES", "202500018"));
+        estudiantesPOO3.add(new Estudiante("LORENZO LOPEZ ERICK GABRIEL", "202500019"));
+        estudiantesPOO3.add(new Estudiante("MACIAS ARTURO LEONARDO DAVID", "202500020"));
+        estudiantesPOO3.add(new Estudiante("MAZA PUNINE ISSAC ALEXANDER", "202500021"));
+        estudiantesPOO3.add(new Estudiante("MEJIA PARRA KEVIN SANTIAGO", "202500022"));
+        estudiantesPOO3.add(new Estudiante("NAVARRETE CASTILLO ANTHONY JOSUE", "202500023"));
+        estudiantesPOO3.add(new Estudiante("POVEDA QUIMIZ MICHAEL CRESCENCIO", "202500024"));
+        estudiantesPOO3.add(new Estudiante("RIVAS ABAD BRAULIO DE JESUS", "202500025"));
+        estudiantesPOO3.add(new Estudiante("RIVAS PINCAY EMMANUEL GERARDO", "202500026"));
+        estudiantesPOO3.add(new Estudiante("ROMERO ALMEIDA EMILIO JOSE", "202500027"));
+        estudiantesPOO3.add(new Estudiante("SANTANDER LOPEZ EDU ISRAEL", "202500028"));
+        estudiantesPOO3.add(new Estudiante("SIAVICHAY VASQUEZ ADRIAN RICARDO", "202500029"));
+        estudiantesPOO3.add(new Estudiante("SUAREZ MENDIETA GARY STEVEN", "202500030"));
+        estudiantesPOO3.add(new Estudiante("SUAREZ VALDIVIESO JOSE JULIO", "202500031"));
+        estudiantesPOO3.add(new Estudiante("VARGAS ISA GENESIS DAYANNA", "202500032"));
+        estudiantesPOO3.add(new Estudiante("VILLAMAGUA ESCUDERO JUAN MATEO", "202500033"));
+        estudiantesPOO3.add(new Estudiante("ZAMORA CEDEÑO JORDY STEVEN", "202500034"));
+        estudiantesPOO3.add(new Estudiante("ZARUMA GAME JOSHUA ANDRES", "202500035"));
+        paraleloPOO3.setEstudiantes(estudiantesPOO3);
+
+        Collections.addAll(Paralelo.paralelos, paraleloCUVP1, paraleloCUVP2, paraleloCUVP3, paraleloCUVP4, paraleloCVP5, paraleloCVP6, paraleloCVP7, paraleloEDP8, paraleloEDP9, paraleloALP10, paraleloALP11, paraleloPOOP12, paraleloPOOP13, paraleloPOOP14, paraleloPOO3);
 
         materia1.agregarParalelo(paraleloCUVP1);
         materia1.agregarParalelo(paraleloCUVP2);
@@ -258,11 +297,47 @@ public class Main {
         ri6.add("Una función que cumple con la propiedad de no linealidad y homogeneidad");
         materia4.agregarPregunta(new Pregunta("¿Qué es una transformación lineal", 1, "Una función que cumple con la propiedad de linealidad y homogeneidad", ri6));
 
+        // P1 Nivel 1
         ArrayList<String> ri7 = new ArrayList<>();
         ri7.add("Un lenguaje de programación");
         ri7.add("Un lenguaje de programación funcional");
         ri7.add("Un lenguaje de programación multiparadigma");
         materia5.agregarPregunta(new Pregunta("¿Qué es Java?", 1, "Un lenguaje de programación orientado a objetos", ri7));
+
+        // P2 Nivel 1
+        ArrayList<String> ri10 = new ArrayList<>();
+        ri10.add("Una clase");
+        ri10.add("Un objeto");
+        ri10.add("Un método");
+        materia5.agregarPregunta(new Pregunta("¿Qué es un objeto?", 1, "Una instancia de una clase", ri10));
+
+        // P3 Nivel 1
+        ArrayList<String> ri11 = new ArrayList<>();
+        ri11.add("Variable global");
+        ri11.add("Variable de instancia");
+        ri11.add("Variable estática");
+        materia5.agregarPregunta(new Pregunta("¿Cuál es el alcance de una variable declarada dentro de un método en Java?", 1, "Variable local", ri11));
+
+        // P1 Nivel 2
+        ArrayList<String> ri21 = new ArrayList<>();
+        ri21.add("Abstracción");
+        ri21.add("Encapsulación");
+        ri21.add("Herencia");
+        materia5.agregarPregunta(new Pregunta("¿Cuáles son los cuatro pilares de la programación orientada a objetos en Java?", 2, "Todas las anteriores", ri21));
+
+        // P2 Nivel 2
+        ArrayList<String> ri22 = new ArrayList<>();
+        ri22.add("public y private");
+        ri22.add("protected y default");
+        ri22.add("abstract y synchronized");
+        materia5.agregarPregunta(new Pregunta("¿Cuáles son los modificadores de acceso en Java?", 2, "Todas las anteriores", ri22));
+
+        // P3 Nivel 2
+        ArrayList<String> ri23 = new ArrayList<>();
+        ri23.add("ArrayList");
+        ri23.add("LinkedList");
+        ri23.add("HashSet");
+        materia5.agregarPregunta(new Pregunta("¿Cuáles son algunas implementaciones de la interfaz Set en Java?", 2, "Todas las anteriores", ri23));
 
         Materia.ingresarMateria(materia1);
         Materia.ingresarMateria(materia2);

11de563965b2a08068944cfa3639848f77ff6ca6
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-08-19 13:14:58 -0500

    Materias y Paralelos menu ui is done TODO: validations and manage backend

--- src/main/java/org/game/MateriasParalelosController.java
+++ src/main/java/org/game/MateriasParalelosController.java
@@ -1,38 +1,39 @@
 package org.game;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 import org.game.lib.Util;
 import org.game.model.data.Materia;
+import org.game.model.data.Paralelo;
 
 public class MateriasParalelosController {
 
     @FXML private StackPane screen;
-    @FXML private VBox materias;
-    @FXML private VBox paralelos;
     @FXML private GridPane gridMaterias;
-    @FXML private Button btnMateria;
+    @FXML private GridPane gridParalelos;
+    @FXML private Button btnAgregarMateria;
+    @FXML private Button btnEditarMateria;
+    @FXML private Button btnAgregarParalelo;
+    @FXML private Button btnEliminarParalelo;
 
     @FXML
     public void initialize()  {
         cargarMaterias();
-    }
-
-    @FXML
-    void menuTerminosAcademicos(MouseEvent event) {
-
+        cargarParalelos();
     }
 
     @FXML
@@ -43,56 +44,34 @@ public class MateriasParalelosController {
     public void cargarMaterias() {
         int i = 1;
         for (Materia materia: Materia.materias) {
-            Label codigo = new Label(materia.getCodigo());
-            codigo.setTextFill(Color.color(1,1,1));
-            gridMaterias.add(codigo, 0, i);
-            Label nombre = new Label(materia.getNombre());
-            nombre.setTextFill(Color.color(1,1,1));
-            gridMaterias.add(nombre, 1, i);
-            Label niveles = new Label(String.valueOf(materia.getNiveles()));
-            niveles.setTextFill(Color.color(1,1,1));
-            gridMaterias.add(niveles, 2, i);
+            Util.addCellToGrid(gridMaterias, materia.getCodigo(), 0, i);
+            Util.addCellToGrid(gridMaterias, materia.getNombre(), 1, i);
+            Util.addCellToGrid(gridMaterias, String.valueOf(materia.getNiveles()), 2, i);
+            i++;
+        }
+    }
+
+    public void cargarParalelos() {
+        int i = 1;
+        for (Paralelo paralelo: Paralelo.paralelos) {
+            Util.addCellToGrid(gridParalelos, paralelo.getMateria().getNombre(), 0, i);
+            Util.addCellToGrid(gridParalelos, paralelo.getTermino().toString(), 1, i);
+            Util.addCellToGrid(gridParalelos, String.valueOf(paralelo.getNumero()), 2, i);
             i++;
         }
     }
 
     public void ingresarMateria() {
-        Util.setButtonStyleClicked(btnMateria);
-        VBox agregarMateria = new VBox();
-        ImageView close = Util.loadIcon("src/main/resources/close.png");
+        Util.setButtonStyleClicked(btnAgregarMateria);
+        VBox agregarMateria = Util.generateModal(screen, btnAgregarMateria);
 
-        assert close != null;
-        close.setOnMouseClicked(e -> {
-            screen.getChildren().remove(agregarMateria);
-            Util.setButtonStyleUnclicked(btnMateria);
-        });
-        close.setStyle("-fx-cursor: hand;");
-        agregarMateria.getChildren().add(close);
+        TextField codigo = Util.addInput(agregarMateria, "Ingrese el código de la materia:");
+        TextField nombre = Util.addInput(agregarMateria, "Ingrese el nombre de la materia:");
+        TextField niveles = Util.addInput(agregarMateria, "Ingrese el número de niveles:");
 
-//        Label codigoLabel = new Label("Ingrese el código de la materia:");
-//        codigoLabel.setTextFill(Color.color(1,1,1));
-//        agregarMateria.getChildren().add(codigoLabel);
-//        TextField codigo = new TextField();
-//        agregarMateria.getChildren().add(codigo);
-        Util.addInput(agregarMateria, "Ingrese el código de la materia:");
-
-        Label nombreLabel = new Label("Ingrese el nombre de la materia:");
-        nombreLabel.setTextFill(Color.color(1,1,1));
-        agregarMateria.getChildren().add(nombreLabel);
-        TextField nombre = new TextField();
-        agregarMateria.getChildren().add(nombre);
-
-        Label nivelesLabel = new Label("Ingrese el número de niveles:");
-        nivelesLabel.setTextFill(Color.color(1,1,1));
-        agregarMateria.getChildren().add(nivelesLabel);
-        TextField niveles = new TextField();
-        agregarMateria.getChildren().add(niveles);
-
-        Button agregar = new Button("Agregar");
-        agregar.setStyle("-fx-background-color: #FE9C33; -fx-cursor: hand;");
-        agregar.setTextFill(Color.color(0, 0, 0));
+        Button agregar = Util.generateButton("Agregar");
         agregar.setOnMouseClicked(e -> {
-            if (codigo.getText().isEmpty() || nombre.getText().isEmpty() || niveles.getText().isEmpty()) {
+            if (codigo.getText().isEmpty() || nombre.getText().isEmpty() || niveles.getText().isEmpty()) { // TODO: validar correctamente
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Error al ingresar la materia");
@@ -100,21 +79,136 @@ public class MateriasParalelosController {
                 alert.showAndWait();
             } else {
                 System.out.println("Agregando materia...");
-                screen.getChildren().remove(agregarMateria);
-                btnMateria.setStyle("-fx-background-color: #042C7D;");
-                btnMateria.setTextFill(Color.color(1, 1, 1));
+                Util.removeModal(screen, agregarMateria, btnAgregarMateria);
             }
 
         });
         agregarMateria.getChildren().add(agregar);
-
-        agregarMateria.setMaxWidth(500);
-        agregarMateria.setMaxHeight(500);
-        agregarMateria.setStyle("-fx-background-color: #042C7D;");
-        screen.getChildren().add(agregarMateria);
     }
 
     public void editarMateria() {
+        Util.setButtonStyleClicked(btnEditarMateria);
+        VBox editarMateria = Util.generateModal(screen, btnEditarMateria);
 
+        Util.addLabelToBox(editarMateria, "Selecciona la forma de editar la materia:");
+        ChoiceBox editarMateriaOpciones = new ChoiceBox();
+        editarMateriaOpciones.getItems().add("Editar usando código");
+        editarMateriaOpciones.getItems().add("Editar usando nombre");
+
+        editarMateria.getChildren().add(editarMateriaOpciones);
+
+        VBox div = new VBox();
+        editarMateria.getChildren().add(div);
+
+        AtomicReference<TextField> field = new AtomicReference<>(new TextField());
+
+        AtomicReference<TextField> nuevoNombre = new AtomicReference<>(new TextField());
+        AtomicReference<TextField> nuevoNumeroNiveles = new AtomicReference<>(new TextField());
+
+        editarMateriaOpciones.setOnAction(e -> {
+            div.getChildren().clear();
+            if (editarMateriaOpciones.getValue().equals("Editar usando código")) {
+                field.set(Util.addInput(div, "Ingrese el código de la materia:"));
+            } else {
+                field.set(Util.addInput(div, "Ingrese el nombre de la materia:"));
+            }
+            nuevoNombre.set(Util.addInput(div, "Ingrese el nuevo nombre de la materia:"));
+            nuevoNumeroNiveles.set(Util.addInput(div, "Ingrese el nuevo número de niveles:"));
+        });
+
+        Button editar = Util.generateButton("Editar");
+        editar.setOnMouseClicked(e -> {
+            if (editarMateriaOpciones.getValue().equals("Editar usando código")) {
+                if (field.get().getText().isEmpty()) { // TODO: validar que el código exista
+                    Util.showAlert("Error al editar la materia", "Por favor, ingrese el código de la materia");
+                } else {
+                    System.out.println("Editando materia...");
+                    Util.removeModal(screen, editarMateria, btnEditarMateria);
+                }
+            } else {
+                if (field.get().getText().isEmpty()) { // TODO: validar que el nombre exista
+                    Util.showAlert("Error al editar la materia", "Por favor, ingrese el nombre de la materia");
+                } else {
+                    System.out.println("Editando materia...");
+                    Util.removeModal(screen, editarMateria, btnEditarMateria);
+                }
+            }
+        });
+
+        editarMateria.getChildren().add(editar);
+    }
+
+    public void agregarParalelo() {
+        Util.setButtonStyleClicked(btnAgregarParalelo);
+        VBox agregarParalelo = Util.generateModal(screen, btnAgregarParalelo);
+
+        TextField materia = Util.addInput(agregarParalelo, "Ingrese el nombre de la materia:");
+        TextField termino = Util.addInput(agregarParalelo, "Ingrese el término académico:");
+        TextField numero = Util.addInput(agregarParalelo, "Ingrese el número de paralelo:");
+
+        Util.addLabelToBox(agregarParalelo, "Seleccione el archivo con los estudiantes:");
+        Button addFile = Util.generateButton("Seleccionar archivo");
+
+        VBox div = new VBox();
+
+        AtomicReference<File> file = new AtomicReference<>(null);
+
+        addFile.setOnMouseClicked(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Seleccionar archivo");
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("CSV", "*.csv")
+            );
+
+            file.set(fileChooser.showOpenDialog(null));
+
+            if (file.get() != null) {
+                div.getChildren().clear();
+                Util.addLabelToBox(div, "Archivo seleccionado: " + file.get().getName());
+            }
+        });
+
+        agregarParalelo.getChildren().add(addFile);
+        agregarParalelo.getChildren().add(div);
+
+        Button agregar = Util.generateButton("Agregar");
+
+        VBox archivoContainer = new VBox();
+        agregarParalelo.getChildren().add(archivoContainer);
+
+        AtomicReference<TextField> rutaArchivo = new AtomicReference<>(new TextField());
+        agregar.setOnMouseClicked(e -> {
+            if (file.get() == null && rutaArchivo.get().getText().isEmpty()) {
+                Util.showInfo("No se ha seleccionado un archivo", "No has seleccionado un archivo\nEn tal caso, escribe la ruta del archivo para subirlo posteriormente");
+                archivoContainer.getChildren().clear();
+                rutaArchivo.set(Util.addInput(archivoContainer, "Ingrese la ruta del archivo:"));
+            } else { // TODO: manage materia add
+                System.out.println("Agregando paralelo...");
+                Util.removeModal(screen, agregarParalelo, btnAgregarParalelo);
+            }
+        });
+
+        agregarParalelo.getChildren().add(agregar);
+    }
+
+    public void eliminarParalelo() {
+        Util.setButtonStyleClicked(btnEliminarParalelo);
+        VBox eliminarParalelo = Util.generateModal(screen, btnEliminarParalelo);
+
+        TextField materia = Util.addInput(eliminarParalelo, "Ingrese el nombre de la materia:");
+        TextField termino = Util.addInput(eliminarParalelo, "Ingrese el término académico:");
+        TextField numero = Util.addInput(eliminarParalelo, "Ingrese el número de paralelo:");
+
+        Button eliminar = Util.generateButton("Eliminar");
+        eliminar.setOnMouseClicked(e -> {
+            if (materia.getText().isEmpty() || termino.getText().isEmpty() || numero.getText().isEmpty()) { // TODO: validar que el paralelo exista
+                Util.showAlert("Error al eliminar el paralelo", "Por favor, ingrese todos los datos");
+            } else {
+                System.out.println("Eliminando paralelo...");
+                Util.removeModal(screen, eliminarParalelo, btnEliminarParalelo);
+            }
+        });
+
+        eliminarParalelo.getChildren().add(eliminar);
     }
 }
--- src/main/java/org/game/lib/Util.java
+++ src/main/java/org/game/lib/Util.java
@@ -2,11 +2,14 @@ package org.game.lib;
 
 import java.io.FileInputStream;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -34,11 +37,71 @@ public class Util {
         button.setTextFill(Color.color(1, 1, 1));
     }
 
-    public static void addInput(VBox box, String label) {
-        Label codigoLabel = new Label(label);
-        codigoLabel.setTextFill(Color.color(1,1,1));
-        box.getChildren().add(codigoLabel);
-        TextField codigo = new TextField();
-        box.getChildren().add(codigo);
+    public static TextField addInput(VBox box, String label) {
+        Util.addLabelToBox(box, label);
+        TextField labelField = new TextField();
+        box.getChildren().add(labelField);
+        return labelField;
+    }
+
+    public static VBox generateModal(StackPane screen, Button button) {
+        VBox modal = new VBox();
+        ImageView close = Util.loadIcon("src/main/resources/close.png");
+
+        assert close != null;
+        close.setOnMouseClicked(e -> {
+            screen.getChildren().remove(modal);
+            Util.setButtonStyleUnclicked(button);
+        });
+        close.setStyle("-fx-cursor: hand;");
+        modal.getChildren().add(close);
+
+        modal.setStyle("-fx-background-color: #042C7D;");
+        modal.setMaxWidth(500);
+        modal.setMaxHeight(500);
+
+        screen.getChildren().add(modal);
+
+        return modal;
+    }
+
+    public static void addLabelToBox(VBox modal, String label) {
+        Label labelTitle = new Label(label);
+        labelTitle.setTextFill(Color.color(1,1,1));
+        modal.getChildren().add(labelTitle);
+    }
+
+    public static Button generateButton(String action) {
+        Button button = new Button(action);
+        button.setStyle("-fx-background-color: #FE9C33; -fx-cursor: hand;");
+        button.setTextFill(Color.color(0, 0, 0));
+        return button;
+    }
+
+    public static void showAlert(String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public static void showInfo(String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public static void addCellToGrid(GridPane grid, String text, int col, int row) {
+        Label content = new Label(text);
+        content.setTextFill(Color.color(1,1,1));
+        grid.add(content, col, row);
+    }
+
+    public static void removeModal(StackPane screen, VBox modal, Button button) {
+        screen.getChildren().remove(modal);
+        Util.setButtonStyleUnclicked(button);
     }
 }
